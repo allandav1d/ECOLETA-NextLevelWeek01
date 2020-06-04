131e7d7d845c4414e577aca1d7b26ee9c085f24e
allandav1d <allan.j.k@hotmail.com>
2020-06-04 18:36:19 -0300

    3º Dia Next Level Week 01

--- web/README.md
+++ web/README.md
@@ -1,44 +1,62 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">
+    <img alt="ECOLETA" title="#ECOLETA - NEXT LEVEL WEEK" src=".github/logo.png" width="250px" />
+</h1>
 
-## Available Scripts
+<h4 align="center">
+  :recycle: ECOLETA - Next Level Week 1
+</h4>
 
-In the project directory, you can run:
+<p align="center">
+ <a href="https://allandavid.com.br">
+    <img alt="Made by AllanDav1d" src="https://img.shields.io/badge/made%20by-AllanDav1d-%237159C1">
+  </a>
+ 
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/allandav1d/ECOLETA-NextLevelWeek01/web">
 
-### `yarn start`
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/allandav1d/ECOLETA-NextLevelWeek01/web">
+  
+  <a href="https://github.com/allandav1d/ECOLETA-NextLevelWeek01/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/allandav1d/ECOLETA-NextLevelWeek01/web">
+  </a>
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+  <a href="https://github.com/allandav1d/ECOLETA-NextLevelWeek01/issues">
+    <img alt="Repository issues" src="https://img.shields.io/github/issues/allandav1d/ECOLETA-NextLevelWeek01">
+  </a>
+</p>
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-como-contribuir">Como contribuir</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
 
-### `yarn test`
+## :rocket: Tecnologias
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Esse projeto foi desenvolvido com as seguintes tecnologias:
 
-### `yarn build`
+- [React](https://reactjs.org)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Node.js](https://nodejs.org/en/)
+- [leaflet](https://leafletjs.com/)
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## :computer: Projeto
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+O Ecoleta é um projeto que conectar pontos de coleta de resíduos com pessoas.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+   Aplicação WEB onde empresas e postos de coleta poderão se cadastrar para ficarem disponiveis na busca do app
 
-### `yarn eject`
+<br><br>
+Aplicação web construída na trilha <strong>Booster</strong> da <strong>Next Level Week 1</strong> distribuída pela [Rocketseat](https://rocketseat.com.br/).
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+## :bookmark: Layout
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+<p align="center">
+  <img alt="Banner-Ecoleta" src=".github/ecoleta-banner-web.svg" width="100%">
+</p>
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+## :thinking: Como contribuir
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+## :memo: Licença
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,232 @@
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+import axios from 'axios';
+import { LeafletMouseEvent } from 'leaflet';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+import { FiArrowLeft, FiCheckCircle } from 'react-icons/fi';
+
+const CreatePoint = () => {
+    //Estado para array ou objeto devemos sempre informar o tipo da variavel
+    interface Item{id:number, title:string, image_url:string}
+    interface IBGEUFResponse {sigla: string; nome:string;}
+    interface IBGECityResponse {nome:string;}
+
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedPosition, setSelectedPosition] = useState<[number,number]>([0,0]);
+    const [initialPosition, setInitialPosition] = useState<[number,number]>([0,0]);
+    const [sucessSubmit, setSucessSubmit] = useState(false);
+    const history = useHistory();
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: ''
+    });
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+
+            setInitialPosition([latitude, longitude]);
+        });
+    }, []);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(response => {
+                const ufInitials = response.data.map(uf => uf.sigla);
+                setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        //Carregar as cidades sempre que a UF mudar
+        if(selectedUf === '0'){
+            return;
+        }
+
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios?orderBy=nome`).then(response => {
+                const cityNames = response.data.map(city => city.nome);
+                setCities(cityNames);
+        });
+    }, [selectedUf]);
+
+    function handleSelectUF(e: ChangeEvent<HTMLSelectElement>){
+        const uf = e.target.value;
+        setSelectedUf(uf);
+    }
+
+    function handleSelectCity(e: ChangeEvent<HTMLSelectElement>){
+        const city = e.target.value;
+        setSelectedCity(city);
+    }
+
+    function handleMapClick(e: LeafletMouseEvent){
+        setSelectedPosition([e.latlng.lat, e.latlng.lng]);
+    }
+
+    function handleInputChange(e: ChangeEvent<HTMLInputElement>){
+        const { name, value }  = e.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    async function handleSubmit(e: FormEvent){
+        e.preventDefault();
+        const { name, email, whatsapp } = formData;
+        const uf =selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name, email, whatsapp, uf, city, latitude, longitude, items
+        };
+
+
+        //await api.post('points', data);
+        setSucessSubmit(true);
+
+        sleep(4000).then(() => { 
+            history.push('/');
+        });
+        //
+    }
+
+    function handleSelectItem(id: number){
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0){
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems);
+        }else{
+            setSelectedItems([...selectedItems, id]);
+        }
+    }
+
+    function sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    return(
+        <div id="page-create-point">
+            <div className={sucessSubmit ? 'is-success':'hide'}>
+                <div className="alert">
+                    <FiCheckCircle className="check-icon"/>
+                    <h2>Cadastro concluído!</h2>
+                </div>
+            </div>
+
+
+            <header>
+                <img src={logo} alt=""/>
+                <Link to="/">
+                    <FiArrowLeft /> Voltar para home
+                </Link>
+            </header>
+
+            <form onSubmit={handleSubmit}>
+                <h1>Cadastro do <br/> ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input type="text" name="name" id="name" onChange={handleInputChange}/>
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" name="email" id="email" onChange={handleInputChange}/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input type="text" name="whatsapp" id="whatsapp" onChange={handleInputChange}/>
+                        </div>
+                    </div>
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no  mapa</span>
+                    </legend>
+
+                    <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+                        <TileLayer 
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={selectedPosition}/>
+                    </Map> 
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select onChange={handleSelectUF} value={selectedUf} name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select onChange={handleSelectCity} value={selectedCity} name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div> 
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                        <span>Selecione um ou mais items a baixo</span>
+                    </legend>
+                    
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li 
+                                key={item.id} 
+                                className={selectedItems.includes(item.id) ? 'selected':''}
+                                onClick={() => handleSelectItem(item.id)}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default CreatePoint;
